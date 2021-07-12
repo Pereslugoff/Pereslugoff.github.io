435a9038c3ab669d53171477d8a749dcd1f2612e
Nick P <nickpereslugoff@gmail.com>
2021-07-11 20:53:11 -0400

    feat(form): prefill form and refactor

--- src/components/ApplyForm.js
+++ src/components/ApplyForm.js
@@ -1,39 +1,75 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import ErrorMessage from './ErrorMessage';
 require('../styles/ApplyForm.css');
+
 const mrBeastHead = require('../images/mr-beast-head.png');
 const chatBubble = require('../images/chat-bubble.svg');
 
 const ApplyForm = () => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [productType, setProductType] = useState('');
+  const [formValues, setFormValues] = useState({
+    fullName: '',
+    email: '',
+    optionalMessage: '',
+    productType: ''
+  });
+  const [chatBubbleMessage, setChatBubbleMessage] = useState('Apply Below!');
   const [isError, setIsError] = useState(false);
-  const [chatBubbleMessage, setChatBubbleMessage] = useState('Apply Now!');
   const [chatBubbleErrorMessages, setChatBubbleErrorMessages] = useState({
     fullName: '',
     email: '',
     productType: ''
   });
 
-  const renderErrorMessages = () => {
-    const errorMessages = [];
-    for(let errorKey in chatBubbleErrorMessages) {
-      if(chatBubbleErrorMessages[errorKey]) {
-        errorMessages.push(
-          <p 
-            key={`${errorKey}Error`}
-            className="applyNowBubbleErrorText"
-          >
-            {chatBubbleErrorMessages[errorKey]}
-          </p>);
-      }
+  const currentLocation = window.location.search;
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(currentLocation);
+    const prefillFullName = queryParams.get('fullName');
+    const prefillEmail = queryParams.get('email');
+
+    if(prefillFullName?.length < 25) {
+        setFormValues({
+          ...formValues,
+          fullName: prefillFullName
+        });
+    } else if(prefillEmail?.length < 25 && /\S+@\S+\.\S+/.test(prefillEmail)) {
+      setFormValues({
+        ...formValues,
+        email: prefillEmail
+      });
+    } 
+  }, [currentLocation]);
+  
+
+  const validate = (formKey, errorMessage, successMessage) => {
+      let isValid;
+      if(!formValues[formKey]) {
+        isValid = false;
+        setIsError(true);
+        setChatBubbleErrorMessages({
+          ...chatBubbleErrorMessages,
+          [formKey]: errorMessage
+        });
+      } else if(formKey === 'email' && !/\S+@\S+\.\S+/.test(formValues.email)) {
+        isValid = false;
+        setIsError(true);
+        setChatBubbleErrorMessages({
+          ...chatBubbleErrorMessages,
+          email: 'Email must be valid'
+        });
+      } else {
+        isValid = true;
+        setChatBubbleMessage(successMessage)
+        if(chatBubbleErrorMessages[formKey]) {
+          setIsError(false);
+          setChatBubbleErrorMessages({
+            ...chatBubbleErrorMessages,
+            [formKey]: ''
+          });
+        }
     }
-    return (
-      <div className="applyNowBubbleErrorTextContainer">
-        {errorMessages}
-      </div>
-    )
+
+    return isValid
   }
 
   return (
@@ -50,109 +86,80 @@ const ApplyForm = () => {
             className="applyNowImg"
           />
           { !isError && <p className="applyNowBubbleText" key={`${chatBubbleMessage}Message`}>{chatBubbleMessage}</p> }
-          { isError && renderErrorMessages() }
+          { isError && <ErrorMessage chatBubbleErrorMessages={chatBubbleErrorMessages}/> }
         </div>
       </div>
       <form 
         className="applyForm"
         onSubmit={e => {
           e.preventDefault();
-          console.log("SUBMITTING THIS INFO", {
-            fullName,
-            email,
-            productType
-          })
+          if(formValues.fullName && formValues.email && formValues.productType) {
+            console.log("SUBMITTING", [
+              formValues.fullName,
+              formValues.email,
+              formValues.productType,
+              formValues.optionalMessage
+            ]);
+          } else {
+            setChatBubbleMessage("Fill out your info first!");
+          }
         }}
       >
-        <label htmlFor="email">Full Name:</label>
+        <label htmlFor="fullName">Full Name:</label>
         <input 
-          type="name" 
-          name="text"
-          maxLength={20}
-          value={fullName}
-          onChange={e => setFullName(e.target.value)}
-          onBlur={e => {
-            if(!fullName) {
-              setIsError(true);
-              setChatBubbleErrorMessages({
-                ...chatBubbleErrorMessages,
-                fullName: 'Please enter your full name.'
-              });
-            } else {
-              setChatBubbleMessage(`Welcome ${fullName}!`)
-              if(chatBubbleErrorMessages.fullName) {
-                setIsError(false);
-                setChatBubbleErrorMessages({
-                  ...chatBubbleErrorMessages,
-                  fullName: ''
-                });
-              }
-            }
-          }}
+          type="text" 
+          name="fullName"
+          maxLength={25}
+          value={formValues.fullName}
+          onChange={e => setFormValues({
+            ...formValues,
+            fullName: e.target.value
+          })}
+          onBlur={() => validate('fullName', 'Please enter your full name.', `Welcome ${formValues.fullName}!`)}
           placeholder="Please enter your full name."
-          required
         />
         <label htmlFor="email">Email Address:</label>
         <input 
           type="email" 
           name="email"
-          value={email}
+          value={formValues.email}
+          maxLength={25}
           placeholder="Please enter your email"
-          onChange={e => setEmail(e.target.value)}
-          onBlur={e => {
-            if(!email) {
-              setIsError(true);
-              setChatBubbleErrorMessages({
-                ...chatBubbleErrorMessages,
-                email: 'Please enter your email.'
-              });
-            } else if(!/\S+@\S+\.\S+/.test(email)) {
-              setIsError(true);
-              setChatBubbleErrorMessages({
-                ...chatBubbleErrorMessages,
-                email: 'Please enter a valid email.'
-              });
-            } else if(chatBubbleErrorMessages.email) {
-              setIsError(false);
-              setChatBubbleErrorMessages({
-                ...chatBubbleErrorMessages,
-                email: ''
-              });
-            }
-          }}
-          required
+          onChange={e => setFormValues({
+            ...formValues,
+            email: e.target.value
+          })}
+          onBlur={() => validate('email', 'Please enter an email', 'Email? Check!')}
         />
+        <label htmlFor="optionalMessage">Tell us why you're choosing Current! (Optional)</label>
+        <textarea
+          name="optionalMessage"
+          placeholder="Overdraft protection, getting paid early, etc..."
+          value={formValues.optionalMessage}
+          maxLength={140}
+          onChange={e => setFormValues({
+            ...formValues,
+            email: e.target.value
+          })}
+        ></textarea>
         <label htmlFor="productType">Which product would you like to apply for?</label>
         <div className="applyFormSubmitLine">
           <select 
             name="productType"
-            value={productType}
-            onChange={e => setProductType(e.target.value)}
-            onBlur={e => {
-              if(!productType) {
-                setIsError(true);
-                setChatBubbleErrorMessages({
-                  ...chatBubbleErrorMessages,
-                  productType: 'Please select a product.'
-                });
-              } else {
-                setChatBubbleMessage(`You selected ${productType}.`)
-                if(chatBubbleErrorMessages.productType) {
-                  setIsError(false);
-                  setChatBubbleErrorMessages({
-                    ...chatBubbleErrorMessages,
-                    productType: ''
-                  });
-                }
-              }
-            }}
+            value={formValues.productType}
+            onChange={e => setFormValues({
+              ...formValues,
+              productType: e.target.value
+            })}
+            onBlur={() => validate('productType', 'Please select a product.', `You selected ${formValues.productType}.`)}
           >
             <option value="">Please select</option>
             <option value="individual">Individual</option>
             <option value="parent">Parent</option>
             <option value="teen">Teen</option>
           </select>
-          <button className="applyFormSubmit">Submit</button>
+          { !isError && <button className="applyFormSubmit">Submit</button> }
+          { isError && <button disabled className="applyFormSubmitDisabled">Oops!</button> }
         </div>
       </form>
     </section>
---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+require('../styles/ApplyForm.css');
+
+const ErrorMessage = ({chatBubbleErrorMessages}) => {
+  const errorMessages = [];
+  for(let errorKey in chatBubbleErrorMessages) {
+    if(chatBubbleErrorMessages[errorKey]) {
+      errorMessages.push(
+        <p 
+          key={`${errorKey}Error`}
+          className="applyNowBubbleErrorText"
+        >
+          {chatBubbleErrorMessages[errorKey]}
+        </p>);
+    }
+  }
+  return (
+    <div className="applyNowBubbleErrorTextContainer">
+      {errorMessages}
+    </div>
+  )
+}
+
+export default ErrorMessage;
