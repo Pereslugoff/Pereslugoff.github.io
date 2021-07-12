fb29ffc9f8c51d429f6912bf57d2fdb68384860f
Nick <nickpereslugoff@gmail.com>
2021-07-11 22:32:31 -0400

    cicd(github-pages): deployment setup

---
+++ build/precache-manifest.86ccc74a63f9c341297f7a45819a4995.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4bc23973a6da6eb09398d2fdf68dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acbfd678858a74730bbd",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5ee9815334126a0ffa94",
+    "url": "/static/css/main.24312c77.chunk.css"
+  },
+  {
+    "revision": "acbfd678858a74730bbd",
+    "url": "/static/js/2.87896663.chunk.js"
+  },
+  {
+    "revision": "5ee9815334126a0ffa94",
+    "url": "/static/js/main.8fd8bc13.chunk.js"
+  },
+  {
+    "revision": "d0ac25ba28884495e36a",
+    "url": "/static/js/runtime~main.3d204cac.js"
+  },
+  {
+    "revision": "571e48b85829224c0b961d952a2ed9e0",
+    "url": "/static/media/chat-bubble.571e48b8.svg"
+  },
+  {
+    "revision": "69954b6787ef92fb584ba69badcfb8a6",
+    "url": "/static/media/current-logo-indigo-2.69954b67.svg"
+  },
+  {
+    "revision": "71ebbce7e9534c07668a4a409df8d978",
+    "url": "/static/media/mr-beast-head.71ebbce7.png"
+  }
+]);
